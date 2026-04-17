b101fbb5ceeb20e02cb72112d7676b82cce6ea52
Haikal Falah <haikalfalah04@gmail.com>
2026-04-17 17:04:54 +0700

    design tampilan board game

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,50 @@ home:  GameScreen(),
 }
 }
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
+const GameScreen({super.key});
+@override
+State<GameScreen> createState() => _GameScreenState();
+}
+class _GameScreenState extends State<GameScreen> {
+
+@override
+void initState() {
+super.initState();
+}
+
 @override
 Widget build(BuildContext context) {
 return Scaffold(
-body: Center(
-child: Text('Game akan ditampilkan di sini'),
+body: Stack(
+children: [
+Positioned(
+top: 50,
+left: 20,
+child: Container(
+padding: const EdgeInsets.all(10),
+decoration: BoxDecoration(
+color: Colors.black54,
+borderRadius: BorderRadius.circular(10),
+),
+child: const Text(
+'Score: 1',
+style: TextStyle(
+color: Colors.white,
+
+fontSize: 24,
+fontWeight: FontWeight.bold,
+
+),
+),
+),//Container
+), // Positioned
+],
 ),
 );
 }
 }
+
 
 
 
